650186c2f6984f41b8919d5463708902703c4cb8
Sedarius-1 <jakubkalinowski1234@gmail.com>
2025-03-01 20:21:25 +0100

    Fixed pon by excluding redis and upped version

--- src/main/java/org/tc/infrastructure/postgres/entities/ApiRouteEntity.java
+++ src/main/java/org/tc/infrastructure/postgres/entities/ApiRouteEntity.java
@@ -1,11 +1,17 @@
 package org.tc.infrastructure.postgres.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 import org.tc.consts.DatabaseConsts;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @Table(name= DatabaseConsts.API_ROUTE, schema = DatabaseConsts.GATEWAY_SCHEMA)
